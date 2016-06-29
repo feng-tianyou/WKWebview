3692f253a94e42c147ca8e2a4090ce1fe3f43b3d
bruce-sha <bu.ru@qq.com>
2016-06-29 15:36:26 +0800

    Initial Commit

---
+++ WKWebview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebview
+//
+//  Created by FTY on 16/6/29.
+//  Copyright © 2016年 FTY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
